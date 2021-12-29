3381489ccdec23fae81b2eb7dea321aa246f0616
chan079 (Chirok Han) <chirok.han@gmail.com>
2021-12-29 16:58:29 +0000

    Use template
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/loedata@9 ba4ea42a-b3d8-4436-8c77-421fe25901ba

--- www/index.php
+++ www/index.php
@@ -1,15 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<title>Data sets for Lectures on Econometrics 4e by Chirok Han</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-</head>
+<?php
 
-<div class="container p-5 my-5">
-<h1>loedata</h1>
+$domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
+$group_name=ereg_replace('([^\.]*)\..*$','\1',$_SERVER['HTTP_HOST']);
+$themeroot='r-forge.r-project.org/themes/rforge/';
+
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>
+<!DOCTYPE html
+	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en   ">
+
+  <head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title><?php echo $group_name; ?></title>
+	<link href="http://<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
+  </head>
+
+<body>
+
+<!-- R-Forge Logo -->
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
+<tr><td>
+<a href="http://r-forge.r-project.org/"><img src="http://<?php echo $themeroot; ?>/imagesrf/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
+</table>
+
+
+<!-- get project title  -->
+<!-- own website starts here, the following may be changed as you like -->
+
+<!-- MMM
+<?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
+$contents = '';
+while (!feof($handle)) {
+	$contents .= fread($handle, 8192);
+}
+fclose($handle);
+echo $contents; } ?>
+MMM -->
+
+<h1>Welcome to loedata</h1>
 
 <h2>R data sets for Lectures on Econometrics 4e by Chirok Han</h2>
 
@@ -24,7 +54,6 @@ install.packages(&quot;loedata&quot;, repos = &quot;http://econ.korea.ac.kr/~chi
 </pre>
 
 <p><a href="http://econ.korea.ac.kr/~chirokhan/book/" target="_blank">Lectures on Econometrics</a> (in Korean)
-</div>
 
+</body>
 </html>
-
